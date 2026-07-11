08e0d01f439f03066cc8f4dbf0c6a34ce7dd14e4
Aman Khandelwal <138006588+Judge0710@users.noreply.github.com>
2026-07-11 16:44:28 +0530

    Add: buy-and-sell-crypto - submission-7

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-7.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) 
+    {
+        int n = prices.size() ;
+        int ans = 0 ;
+        int temp = 0 ;
+        for (int i = 0 ; i < n ; i++)
+        {
+            for (int j = i+1 ; j < n ; j++)
+            {
+                temp = prices[j] - prices[i] ;
+                ans = max(ans,temp);
+            }
+        }
+        return ans;
+    }
+};
