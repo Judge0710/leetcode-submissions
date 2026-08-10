d01602a1adf337485da11872815126293cf411a6
Aman Khandelwal <138006588+Judge0710@users.noreply.github.com>
2026-08-10 14:22:51 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,38 @@
+class MinStack {
+private:
+    std::stack<int> stack;
+    std::stack<int> minStack;
+
+public:
+    MinStack() {}
+
+    void push(int val) 
+    {
+        if (stack.empty())
+        {
+            stack.push(val);
+            minStack.push(val);
+        }
+        else
+        {
+            stack.push(val);
+            minStack.push(std::min(val, minStack.top()));
+        }
+    }
+
+    void pop() 
+    {
+        stack.pop();
+        minStack.pop();
+    }
+
+    int top() 
+    {
+        return stack.top();
+    }
+
+    int getMin() 
+    {
+        return minStack.top();
+    }
+};
