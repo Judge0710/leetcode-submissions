c5513e5c0527cd97ae04e6e9dd55d206b48b6202
Aman Khandelwal <138006588+Judge0710@users.noreply.github.com>
2026-09-20 11:38:04 +0530

    Add: copy-linked-list-with-random-pointer - submission-2

---
+++ Data Structures & Algorithms/copy-linked-list-with-random-pointer/submission-2.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    Node* copyRandomList(Node* head) 
+    {
+        if(!head)
+        {
+            return head;
+        }
+        Node* curr = head;
+        Node* copyList = new Node(0);
+        Node* ans = copyList;
+        
+        unordered_map<Node*, Node*> mp;
+
+        while (curr)
+        {
+            copyList->val = curr->val;
+            mp[curr] = copyList;
+
+            if (curr->next)
+                copyList->next = new Node(0);
+
+            copyList = copyList->next;
+            curr = curr->next;
+        }
+
+        mp[nullptr] = nullptr;
+
+        curr = head;
+        copyList = ans;
+
+        while (curr)
+        {
+            copyList->random = mp[curr->random];
+            curr = curr->next;
+            copyList = copyList->next;
+        }
+
+        return ans;
+    }
+};
